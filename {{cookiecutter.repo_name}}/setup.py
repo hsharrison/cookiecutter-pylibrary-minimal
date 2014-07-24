80fd700ed8615441640d2e811d85c1b64334cfa0
hsharrison <henry.schafer.harrison@gmail.com>
2014-07-24 13:36:33 -0400

    space out setup.py

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -25,16 +25,20 @@ setup(
     name="{{ cookiecutter.distribution_name }}",
     version=namespace['__version__'],
     license="BSD",
+
     description="{{ cookiecutter.project_short_description }}",
     long_description="%s\n%s" % (read("README.rst"), re.sub(":obj:`~?(.*?)`", r"``\1``", read("CHANGELOG.rst"))),
+
     author="{{ cookiecutter.full_name }}",
     author_email="{{ cookiecutter.email }}",
     url="https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.repo_name }}",
+
     packages=find_packages("src"),
     package_dir={"": "src"},
     py_modules=[splitext(basename(i))[0] for i in glob.glob("src/*.py")],
     include_package_data=True,
     zip_safe=False,
+
     classifiers=[
         # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
         "Development Status :: 3 - Alpha",
@@ -50,12 +54,14 @@ setup(
     keywords=[
         # eg: "keyword1", "keyword2", "keyword3",
     ],
+
     install_requires=[
         # eg: "aspectlib==1.1.1", "six>=1.7",
     ],
     extras_require={
         # eg: 'rst': ["docutils>=0.11"],
     },
+    
     entry_points={
         "console_scripts": [
             "{{ cookiecutter.package_name }} = {{ cookiecutter.package_name }}.__main__:main"
